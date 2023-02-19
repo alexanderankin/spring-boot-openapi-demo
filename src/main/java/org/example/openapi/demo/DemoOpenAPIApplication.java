1be0ce5291010799613c59290a02c5455e8110e8
David Ankin <alexanderankin@gmail.com>
2023-02-18 22:25:12 -0500

    generate project

---
+++ src/main/java/org/example/openapi/demo/DemoOpenAPIApplication.java
@@ -0,0 +1,13 @@
+package org.example.openapi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOpenAPIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOpenAPIApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/openapi/demo/DemoOpenAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.openapi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOpenAPIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
